966f63b4f474b8a5bb8ee980013fab31bd71d402
Marcelo Castellano <marce.geek22@gmail.com>
2020-04-17 17:09:18 -0300

    Fix modification time message

--- show-date.sh
+++ show-date.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 date_str=$(git-file-lastdate/git-file-lastdate.sh -d README.md)
-LD_PRELOAD=libfaketime/src/libfaketime.so.1 FAKETIME=$date_str sh -c 'echo README.md was made at $(date)'
+LD_PRELOAD=libfaketime/src/libfaketime.so.1 FAKETIME=$date_str sh -c 'echo The last modification time of README.md is $(date)'
